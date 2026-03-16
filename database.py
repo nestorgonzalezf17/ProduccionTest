93f6d8507b12ab38fe35fb352f650ebcaf077d58
RealLion <nestorgonzalezf17@gmail.com>
2026-03-16 18:41:21 -0500

    Buenasss v1.2

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 # Construir la URL de conexión
-DB_URL = f"mysql+pymysql://ConApiPY:UZk/(PXEz6UOMw1W@localhost:3306/prueba"
+DB_URL = f"mysql+pymysql://root:rcNZzbGMUEqYiiBAjThIcVfCxyvdVDiC@localhost:3306/railway"
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
